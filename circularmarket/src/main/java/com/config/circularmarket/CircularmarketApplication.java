ab07d10388f922fe0a0105fd330bb4230f5c7ac2
angelnietoo <aniecor1811@g.educaand.es>
2026-02-03 11:13:55 +0100

    Proyecto Springboot

---
+++ circularmarket/src/main/java/com/config/circularmarket/CircularmarketApplication.java
@@ -0,0 +1,13 @@
+package com.config.circularmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircularmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircularmarketApplication.class, args);
+	}
+
+}
